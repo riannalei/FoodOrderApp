1ae8f92b73ee87e3327683feec0c316a155b3d03
riannalei <riannalei@gmail.com>
2024-07-26 23:51:05 -0700

    modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Header from './components/Header.jsx'; 
-import Meals from './components/Meals.jsx';
+import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
+import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx"; // Import the correct provider
+import Cart from "./components/Cart.jsx";
+import Checkout from "./components/Checkout.jsx";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Meals />
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header title="React Food App" />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
-import Button from './UI/Button.jsx'; 
-import logoImg from '../assets/logo.jpg';
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
-export default function Header() {
-    return (
+export default function Header({ title }) {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+  return (
     <header id="main-header">
-        <div id ="title">
-            <img src ={logoImg} alt = "A resturant" />
-            <h1>ReactFood</h1>
-        </div>
-        <nav>
-            <Button textOnly>Cart (0)</Button>
-        </nav>
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>{title}</h1>
+      </div>
+      <nav>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
+      </nav>
     </header>
-    ); 
+  );
 }
